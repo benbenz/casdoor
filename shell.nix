8a9bd4528e3c163ba349a0c73c8fa5492dd0baa5
benbenz <45265258+benbenz@users.noreply.github.com>
2024-09-02 15:21:08 +0200

    Create shell.nix

---
+++ shell.nix
@@ -0,0 +1,18 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.git
+    pkgs.nodejs_18
+    pkgs.certbot
+    pkgs.jre
+    pkgs.python39
+    pkgs.openssl
+    pkgs.postgresql
+    pkgs.go_1_22
+    pkgs.yarn
+  ];
+}
